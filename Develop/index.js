05802f3e5441572cdcfabf364a878215f4991191
marikadicarlo <mdicarlo19@yahoo.com>
2022-01-23 18:03:09 -0500

    created array of questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,84 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the title of your page?",
+        name: "Title",
+      },
+      {
+        type: "input",
+        message: "Please enter a description of your project.",
+        name: "Description",
+      },
+      {
+        typ: "input",
+        message: "Installation Instructions",
+        name: "Installation",
+        default: "No installation information available.",
+      },
+      {
+        type: "input",
+        message: "Usage Information",
+        name: "Usage",
+        default: "No usage information is available.",
+      },
+      {
+        type: "input",
+        message: "Contribution Guidelines",
+        name: "Contribution",
+        default: "No contribution information is available.",
+      },
+      {
+        type: "input",
+        message: "Test Instructions",
+        name: "Test",
+        default: "No testing information is available.",
+      },
+      {
+        type: "list",
+        name: "License",
+        message: "Choose a License..",
+        choices: ["MIT", "ISC", "Unlicense"],
+      },
+      {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "Github",
+        filter: function (answers) {
+            if (answers) {
+                return 'https://github.com/${answers}';
+            } else {
+                return "Please enter valid GitHub username.";
+            }
+        },
+      },
+      {
+          type: "input",
+          message: "What is your email address?",
+          name: "Email",
+          filter: function (answers) {
+              if (answers) {
+                  return '${answers}';
+              } else {
+                  return "No valid email address was entered.";
+              }
+          },
+      },
+    ]);
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
